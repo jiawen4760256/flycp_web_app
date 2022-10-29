e7567f0077689d8448eafba239e4da3642013edd
jiawen <jiawen4760256@gmail.com>
2022-10-29 16:41:13 +0800

    sm

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -248,7 +248,7 @@ export default () => {
 						</Grid.Item>
 					</Grid>
 				
-					<Image className='sc-youhui'  src="/sc/tjpq.png" />
+					<Image className='sc-youhui'  src="/sc/tj.png" />
 					<Grid columns={4} gap={10} className='sc-type-list'>
 						<Grid.Item className='sc-type'  onClick={()=>{navigate("/mall/1")}}>
 							<Image className='sc-type-img' src="/sc/type-dy.png" />
@@ -263,7 +263,7 @@ export default () => {
 							<Image className='sc-type-img' src="/sc/type4.png" />
 						</Grid.Item>
 					</Grid>
-					<Image className='sc-youhui'  src="/sc/sdyx.png" onClick={()=>{navigate("/mall/0")}} />
+					<Image className='sc-youhui'  src="/sc/sm.png" onClick={()=>{navigate("/mall/0")}} />
 					{/* <br/> */}
 					{/* <Image className='sc-jxhw'  src="/sc/jxhw.png" /> */}
 					<Divider className='dy-type'>正在热映</Divider>
